6ee8d50be0f3d3bbd77ca5490407aa3ac918a682
leonirmalraj <leo@cloudrevelinnovation.com>
2024-04-22 18:59:46 +0530

    the client side url updated

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -135,7 +135,7 @@ const forgotPassword = async (req, res) => {
         <p>Dear ${user.firstName} ${user.lastName},</p>
         <p>We received a request to reset your password. Here is your One-Time Password (OTP): <strong>${OTP}</strong></p>
         <p>Please click the following link to reset your password:</p>
-        <a href="https://main--tubular-lokum-952508.netlify.app/reset-password">Reset Password</a>
+        <a href="https://prismatic-chimera-53966a.netlify.app/reset-password">Reset Password</a>
         <p>If you did not make this request, please ignore this email.</p>
         <p>Thank you</p>
         <p>From Validation</p>
